b06ab5f0dabb3ae9b3c283015dc54f6a2af7511f
AorsiniYT <aorsinigamer@gmail.com>
2025-09-02 00:33:26 -0600

    Actualizar el script de instalación para usar box86 y box64 en el proceso de boot de Wine

--- scripts/install-forest.sh
+++ scripts/install-forest.sh
@@ -3,7 +3,7 @@ echo '#!/bin/bash
 box64 wine64 "$@"' > /usr/local/bin/wine-wrapper
 chmod +x /usr/local/bin/wine-wrapper
 export WINE=/usr/local/bin/wine-wrapper
-wine wineboot && wine64 wineboot
+box86 wine wineboot && box64 wine64 wineboot
 xvfb-run sh -c "winetricks -q vcrun2019"
 echo "Starting SteamCMD installation..."
 wine64 /steamcmd/steamcmd.exe +force_install_dir Z:/theforest +login anonymous +app_update 556450 +quit
