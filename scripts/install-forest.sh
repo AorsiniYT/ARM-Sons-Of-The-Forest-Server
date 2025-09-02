c382964d033fb974592a01d8cb77349d4d8ebe2e
AorsiniYT <aorsinigamer@gmail.com>
2025-09-01 23:57:26 -0600

    Actualizar el ID de la aplicación en el script de instalación de SteamCMD para The Forest

--- scripts/install-forest.sh
+++ scripts/install-forest.sh
@@ -2,7 +2,7 @@ export PATH=/usr/local/bin:$PATH
 wine wineboot && wine64 wineboot
 xvfb-run sh -c "winetricks -q vcrun2019"
 echo "Starting SteamCMD installation..."
-wine64 /steamcmd/steamcmd.exe +force_install_dir Z:/theforest +login anonymous +app_update 242760 validate +quit
+wine64 /steamcmd/steamcmd.exe +force_install_dir Z:/theforest +login anonymous +app_update 556450 validate +quit
 echo "SteamCMD finished. Checking installation..."
 ls -la /theforest
 echo "Checking steamapps..."
