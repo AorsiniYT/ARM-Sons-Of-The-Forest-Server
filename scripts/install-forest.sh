3d86eedd716e60be9f36dc055c45586fedffbf74
AorsiniYT <aorsinigamer@gmail.com>
2025-09-02 00:30:46 -0600

    Agregar variable de entorno WINE para compatibilidad con box64 en el script de instalación de The Forest

--- scripts/install-forest.sh
+++ scripts/install-forest.sh
@@ -1,5 +1,6 @@
 export PATH=/usr/local/bin:$PATH
 wine wineboot && wine64 wineboot
+export WINE="box64 wine64"
 xvfb-run sh -c "winetricks -q vcrun2019"
 echo "Starting SteamCMD installation..."
 wine64 /steamcmd/steamcmd.exe +force_install_dir Z:/theforest +login anonymous +app_update 556450 +quit
